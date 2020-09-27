910f99c93818e7b268191746970ddd621a54baa1
TollesDJones <49727120+TollesDJones@users.noreply.github.com>
2020-09-27 14:03:28 -0400

    Initial commit

---
+++ Chapter 5 Demo.py
@@ -0,0 +1,9 @@
+# Mr. Jones
+# This is a sample Python script Based on CH.4 material
+# ASCII text generated with http://www.network-science.de/ascii/
+# Donte Jones jones_donte@dublinschools.net
+# IT Academy @ Emerald Campus
+#
+# Notes PYCHARM users: Use 'CTRL+/' keys to uncomment or comment any selected lines of code
+
+
