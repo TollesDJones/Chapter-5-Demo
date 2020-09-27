f1aab1d9381fad12329692db9e2f6fb741fbf5e2
TollesDJones <49727120+TollesDJones@users.noreply.github.com>
2020-09-27 14:36:14 -0400

    Update Lists section

--- Chapter 5 Demo.py
+++ Chapter 5 Demo.py
@@ -10,5 +10,24 @@
 """
 Lists
 
+Lists are another type of sequence that is very similar to 'Tuples'
+Any procedure or operator we use with Tuples will work on a list
 
+The biggest difference between lists and Tuples is 'Lists' are Mutable
+NOTES
+1. A Tuple can be concattenated another Tuple
+2. You can concatenate a Tuple to a list
+3. You can not concatenate a list to a Tuple
 """
+
+# A list can be created and populated using the following syntax
+# Example 1
+# list_name = ["item1", "item2", "item3", "item4"]
+#             # index 0, index 1, index 2, index 3
+
+
+# Example 2
+len(list_name) # Returns 4 if using the example above
+
+# Slicing a list works the same list_name[:] !End point is exclusive
+
